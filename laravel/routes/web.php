0867ab9d0d556504e4a5a4120a7f75eea620055b
Brett Nielsen <brett@Bretts-MacBook-Air.local>
2020-01-30 10:31:01 -0700

    chnage gets to posts

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -20,25 +20,25 @@ Route::group(['prefix' => '/customer'], function() {
     Route::get('index', 'CustomerController@index');
     Route::get('new', 'CustomerController@new');
     Route::get('edit/{id}', 'CustomerController@edit');
-    Route::get('store', 'CustomerController@store');
-    Route::get('update/{id}', 'CustomerController@update');
-    Route::get('destroy/{id}', 'CustomerController@destroy');
+    Route::post('store', 'CustomerController@store');
+    Route::post('update/{id}', 'CustomerController@update');
+    Route::post('destroy/{id}', 'CustomerController@destroy');
 });
 
 Route::group(['prefix' => '/item'], function() {
     Route::get('index', 'ItemController@index');
     Route::get('new', 'ItemController@new');
     Route::get('edit/{id}', 'ItemController@edit');
-    Route::get('store', 'ItemController@store');
-    Route::get('update/{id}', 'ItemController@update');
-    Route::get('destroy/{id}', 'ItemController@destroy');
+    Route::post('store', 'ItemController@store');
+    Route::post('update/{id}', 'ItemController@update');
+    Route::post('destroy/{id}', 'ItemController@destroy');
 });
 
 Route::group(['prefix' => '/reservation'], function() {
     Route::get('index', 'ReservationController@index');
     Route::get('new', 'ReservationController@new');
     Route::get('edit/{id}', 'ReservationController@edit');
-    Route::get('store', 'ReservationController@store');
-    Route::get('update/{id}', 'ReservationController@update');
-    Route::get('destroy/{id}', 'ReservationController@destroy');
+    Route::post('store', 'ReservationController@store');
+    Route::post('update/{id}', 'ReservationController@update');
+    Route::post('destroy/{id}', 'ReservationController@destroy');
 });
