fafa52f9ce02a66c064b99e4f3c085e79f2ebf20
Brett Nielsen <brett@Bretts-MacBook-Air.local>
2020-02-27 11:35:58 -0700

    add logo to header and create footer

---
+++ laravel/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer style="position: fixed; bottom: -10px; width: 100vw; height: 50px; background-color: white;">
+    <div style="text-align: center; box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);">
+        <img src="{{ url("systemImages/logo.png") }}" alt="Tool Co-op" style="max-height: 50px; max-width: 100px;">
+    </div>
+</footer>
