77d7c78c5e7b44eff6d9f23f1b26310d3be812f3
Cade Rasmussen <rascade@gmail.com>
2020-02-27 11:24:17 -0700

    starting working on the terms and agreements page and the help page .

--- laravel/resources/views/help.blade.php
+++ laravel/resources/views/help.blade.php
@@ -1,10 +1,27 @@
-@section('title', 'Help')
+@section('title', 'Information')
 
 @extends('layouts.app')
 
-@section('cardTitle', 'Help')
+@section('cardTitle', 'Information')
 
 @section('content')
-    <p class="contianerClass">This is where the html goes</p>
-
+    <h1>Common Questions</h1>
+    <h2>How to create an account on our website</h2>
+        <ol>
+            <li>visit www.tool-coop.com</li>
+            <li>click register in the upper right hand corner of the page</li>
+            <li>fill out the required information and click create account</li>
+        </ol>
+    <h2>If I damage a tool when in use do I have to pay?</h2>
+        <p class="containerClass">     item number two of <a href="/terms">terms and agreements</a></p>
+    <h2>If I don't use computers can I still access tools from your warehouse?</h2>
+        <p>     Yes, our company exists to allow ease of access to all members of our community and we
+            will do our best to give this service to all who wants it. Just come to our location and upon
+            checkout of desired tools our friendly assistant will set up your account for you!</p>
+    <h2>Contact info</h2>
+    <ul>
+        <li>Location: 852 MapleBerry Drive</li>
+        <li>Phone number: (435) 777-4568 </li>
+        <li>Email: toolCoopLuvs@gmail.com</li>
+    </ul>
 @endsection
--- laravel/resources/views/terms.blade.php
+++ laravel/resources/views/terms.blade.php
@@ -5,6 +5,13 @@
 @section('cardTitle', 'Terms and conditions')
 
 @section('content')
-    <p>This is where the html goes</p>
+    <p>Users are required to abide by the following rules of access and code of conduct</p>
+    <ol>
+        <li>Use tools as if they were your own</li>
+        <li>If tools break while in use, the company is liable</li>
+        <li>client may not have checked out tools for longer than 3 months</li>
+        <li>if client does not return tool, future reservations will be blocked until tool is returned</li>
+        <li>tools lended by the company causing injury to client during use is liable to the client</li>
+    </ol>
 
 @endsection
