f32b3d1a5427362d09144eaba7921bc0ccb15c20
Cade Rasmussen <rascade@gmail.com>
2020-03-06 13:55:16 -0700

    tabbed one thing over

--- laravel/resources/views/help.blade.php
+++ laravel/resources/views/help.blade.php
@@ -15,7 +15,7 @@
     <h2>If I damage a tool when in use do I have to pay?</h2>
         <p class="containerClass">     item number two of <a href="/terms">terms and agreements</a></p>
     <h2>If I don't use computers can I still access tools from your warehouse?</h2>
-        <p>     Yes, our company exists to allow ease of access to all members of our community and we
+        <p style="text-indent: 30px">Yes, our company exists to allow ease of access to all members of our community and we
             will do our best to give this service to all who wants it. Just come to our location and upon
             checkout of desired tools our friendly assistant will set up your account for you! He will show
             you what the app can do and you can use it in the future or always just walk in to make your desired
